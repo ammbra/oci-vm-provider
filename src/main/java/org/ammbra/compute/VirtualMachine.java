a864661b9d7cb3f235e4ada34ed4c8b46644c4b0
Ana <mihalceanuana@gmail.com>
2023-12-13 12:04:16 +0100

    Remove unused record

--- src/main/java/org/ammbra/compute/VirtualMachine.java
+++
@@ -1,8 +0,0 @@
-package org.ammbra.compute;
-
-import com.pulumi.core.Output;
-import org.ammbra.compute.finder.PlatformImage;
-import org.ammbra.compute.subnet.Subnetwork;
-
-public record VirtualMachine(Output<String> displayName, Subnetwork subnet, PlatformImage imageName){
-}
